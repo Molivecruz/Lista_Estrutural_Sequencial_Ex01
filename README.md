00207ef44aaef82c83c6329749ff2cd831db4a51
Molivecruz <molivecruz@gmail.com>
2022-10-04 13:19:14 -0300

    Upload do Ex01

---
+++ README.md
@@ -0,0 +1,10 @@
+## Projeto Modelo para Atividade Java X GitHub
+
+Seja bem-vindo ao projeto.
+
+Este é o exercício 01.
+
+## Enunciado
+
+Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Requisito 01: Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+        System.out.println("Alô, Mundo!");
+    }
+}
